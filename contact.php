17a185449d6897ba179ab8ef04bd0a955502f287
lucagao <61145859+lucagao@users.noreply.github.com>
2020-02-17 23:56:16 +0100

    Update contact.php
    php3

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
 
 $name = $_POST['name'];
+$lastname = $-POST[ 'lastname'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
@@ -10,6 +11,7 @@ $email_from = 'gao20@hotmail.it';
 $email_subject = "New Form Submission";
 
 $email_body = "User Name: $name.\n".
+"User lastname: $lastname.\n".
 "User Email: $visitor_email.\n".
 "User Message: $message.\n";
 
